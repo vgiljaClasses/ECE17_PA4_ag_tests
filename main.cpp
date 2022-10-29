b549364d99fd46cfa8865d95ac222349a417657e
vgilja <vgilja@ucsd.edu>
2022-10-28 21:28:58 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -20,33 +20,39 @@ int main(int argc, const char * argv[]) {
     std::string temp(argv[1]);
     if("compile"==temp) {
       std::cout << "Passed Compile Test\n";
+      return 0;
     }
     else if("basic"==temp) {
       if(!doBasicTest()) {
         std::cout << "Passed Basic Test\n";
+        return 0;
       }
     }
     else if ("random"==temp) {
       if(!doRandomTest()) {
         std::cout << "Passed Random Test\n";
+        return 0;
       }
     }
     else if ("visitor"==temp) {
       if(!doVisitorTest()) {
         std::cout << "Passed Visitor Test\n";
+        return 0;
       }
     }
     else if ("stack"==temp) {
       if(!doStackTest()) {
         std::cout << "Passed Stack Test\n";
+        return 0;
       }
     }
     else if ("queue"==temp) {
       if(!doQueueTest()) {
         std::cout << "Passed Queue Test\n";
+        return 0;
       }
     }    
-    return 0;
+    return 1;
   }
   
   //STUDENT -- add your own additional tests here...
